caa0696285f21ef65ed519edc620349af6f07c0b
Faruk Mašinović <paytime@users.noreply.github.com>
2019-11-09 18:10:45 +0100

    Admins can always set any roles

--- commands/ToggleRole.js
+++ commands/ToggleRole.js
@@ -2,6 +2,8 @@ module.exports = {
     name: 'toggle',
     description: 'Toggles the role of a user. Syntax: `@bot Toggle @player @ROLE`',
     execute(msg, args, list) {
+        const isAdmin = msg.member.hasPermission('ADMINISTRATOR');
+
         // Check if the args are valid.
         if (args.length !== 2) {
             throw new Error('Wrong number of args.');
@@ -33,11 +35,16 @@ module.exports = {
 
         // Check if the message's author has the permission to toggle this role
         let hasPermission = false;
-        list.forEach(ele => { // Check if 
-            if (msg.member.roles.has(ele.assigner) && ele.assignableRoles.includes(roleId)) {
-                hasPermission = true;
-            }
-        });
+
+        if(isAdmin) { // Admins have always the permission to change roles
+            hasPermission = true;
+        } else {
+            list.forEach(ele => { // Check if 
+                if (msg.member.roles.has(ele.assigner) && ele.assignableRoles.includes(roleId)) {
+                    hasPermission = true;
+                }
+            });
+        }
 
         // Finally, toggle the player's role
         if (hasPermission) {
